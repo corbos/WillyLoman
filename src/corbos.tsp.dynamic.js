cce3e0d78e85eb9e5f2d67f802fe09731c78f4ee
corbos <corbin.march@gmail.com>
2014-10-28 17:00:23 -0500

    Changed dynamic algorithm to iterate versus recursion. Doesn't lock up a browser.

--- src/corbos.tsp.dynamic.js
+++ src/corbos.tsp.dynamic.js
@@ -1,17 +1,6 @@
 (function (willy) {
     'use strict';
 
-    var Dynamic = function (solver) {
-        this.solver = solver;
-        this.done = true;
-        this.distances = [];
-        this.cache = {};
-        this.initDistances();
-    };
-
-    willy.extendAlgorithm(Dynamic, 'BranchNBound', 'initDistances');
-    willy.extendAlgorithm(Dynamic, 'BranchNBound', 'lookupDistance');
-
     var sortInt = function (a, b) { return a - b; };
 
     var getKey = function (indexes) {
@@ -19,47 +8,83 @@
         return indexes.join("_");
     };
 
-    Dynamic.prototype.minDistance = function (start, rest) {
-
-        var best = {
-                distance: Number.MAX_VALUE,
-                path:[]
-            },
-            next,
-            len = rest.length,
-            result,
-            key,
-            d = 0.0;
-
-        if (len === 1) {
-            return {
-                distance: this.lookupDistance(start, rest[0]) + this.lookupDistance(0, rest[0]),
-                path: [rest[0], start]
-            };
-        }
-
-        key = start.toString() + '_' + getKey(rest);
-        if (this.cache[key] !== undefined)
-            return this.cache[key];
-
-        for (var i = 0; i < len; i++) {
-            next = rest.slice(0, i).concat(rest.slice(i + 1));
-            result = this.minDistance(rest[i], next);
-            d = this.lookupDistance(start, rest[i]) + result.distance;
-            if (d < best.distance) {
-                best.distance = d;
-                best.path = result.path.concat(start);
-            }
-        };
-
-        this.cache[key] = best;
-
-        return best;
+    var InstructionState = function (start, rest) {
+        this.start = start;
+        this.rest = rest;
+        this.path = [];
+        this.key = start.toString() + "_" + getKey(rest);
+        this.distance = Number.MAX_VALUE;
     };
 
+    var InstructionSet = function () {
+        this.toDo = [];
+        this.inProgress = [];
+        this.done = [];
+    };
+
+    var Dynamic = function (solver) {
+        this.solver = solver;
+        this.distances = [];
+        this.cache = {};
+        this.initDistances();
+        var instructionSet = new InstructionSet();
+        instructionSet.toDo.push(new InstructionState(0, solver.getPossible().slice(1)));
+        this.instructions = [instructionSet];
+    };
+
+    willy.extendAlgorithm(Dynamic, 'BranchNBound', 'initDistances');
+    willy.extendAlgorithm(Dynamic, 'BranchNBound', 'lookupDistance');
+
     Dynamic.prototype.solve = function () {
-        var result = this.minDistance(0, this.solver.getPossible().slice(1));
-        return result.path;
+
+        var current, toDo, count = 0, i, cached,
+            len, inProgress, d, instructionSet;
+
+        while (count++ < 1000) {
+
+            current = this.instructions[this.instructions.length - 1];
+            toDo = current.toDo.pop();
+
+            if (toDo) {
+                len = toDo.rest.length;
+                if (len === 1) {
+                    toDo.distance = this.lookupDistance(toDo.start, toDo.rest[0]) + this.lookupDistance(0, toDo.rest[0]);
+                    toDo.path = [toDo.rest[0], toDo.start];
+                    current.done.push(toDo);
+                } else if ((cached = this.cache[toDo.key]) !== undefined) {
+                    current.done.push(cached);
+                } else {
+                    current.inProgress.push(toDo);
+                    instructionSet = new InstructionSet();
+                    for (i = 0; i < len; i++) {
+                        instructionSet.toDo.push(
+                                new InstructionState(
+                                    toDo.rest[i],
+                                    toDo.rest.slice(0, i).concat(toDo.rest.slice(i + 1))
+                                    )
+                                );
+                    }
+                    this.instructions.push(instructionSet);
+                }
+            } else if (this.instructions.length === 1) {
+                this.done = true;
+                return current.done[0].path;
+            } else {
+                this.instructions.pop();
+                inProgress = this.instructions[this.instructions.length - 1].inProgress.pop();
+                current.done.forEach(function (item) {
+                    d = this.lookupDistance(inProgress.start, item.start) + item.distance;
+                    if (d < inProgress.distance) {
+                        inProgress.distance = d;
+                        inProgress.path = item.path.concat(inProgress.start);
+                    }
+                }, this);
+                this.cache[inProgress.key] = inProgress;
+                this.instructions[this.instructions.length - 1].done.push(inProgress);
+            }
+        }
+
+        return this.solver.getPossible();
     };
 
     willy.register({
